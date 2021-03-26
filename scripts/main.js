65d96fdaabd22ade7e37e6696fb9218a2a97cde1
Blaz Vrataric <online.blaz@gmail.com>
2021-03-26 12:25:56 +0100

    day 1 progress of doing the exercise

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+let number1
+let number2
+function add(number1, number2) {
+    return number1 + number2;
+}
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+function divide(number1, number2) {
+    return number1 / number2;
+}
+function operate(operator, number1, number2) {
+    return operator(number1, number2);
+}
