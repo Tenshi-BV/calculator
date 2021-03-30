919ed8844daa88308d94ef5036df1e35e056adfd
Blaz Vrataric <online.blaz@gmail.com>
2021-03-30 10:16:57 +0200

    the algorithm is done

--- scripts/main.js
+++ scripts/main.js
@@ -35,10 +35,9 @@ function operate(operator, number1, number2) {
 
 // DISPLAY FUNCTIONS
 function keyPress(x) {
-    x = x.toString();
     if (displayValue == 0) {
         displayValue = x;
-    } else {
+    } else  if (displayValue.length < 12) {
         displayValue = displayValue + x;
     }
     updateDisplay();
@@ -67,18 +66,17 @@ function undoAll() {
 function operatorAssignment(x) {
     if (number1 == undefined) {
     number1 = Number(displayValue);
+    } else if (displayValue != 0) {
+        calculate();
+    }
     displayValue = "0";
     operator = x;
-    } else {
-        calculate()
-        operator = x;
-    }
 }
 
 function dotPress() {
     if (displayValue.search(/\./) >= 0) {
         return;
-    } else {
+    } else if (displayValue.length < 11) {
         displayValue = displayValue + ".";
     }
     updateDisplay();
@@ -86,16 +84,15 @@ function dotPress() {
 
 function calculate() {
     if (operator == undefined) {
-        number1 = Number(displayValue);
-        displayValue = "0";
+        return;
     } else {
         number2 = Number(displayValue);
         displayValue = operate(operator, number1, number2);
+        displayValue = roundUp(displayValue);
         number1 = displayValue;
         operator = undefined;
         displayValue = displayValue.toString();
         updateDisplay();
-        displayValue = "0";
     }
 }
 
@@ -105,7 +102,20 @@ function flip() {
     } else if (displayValue.search("-") < 0) {
         displayValue = "-" + displayValue;
     }
+    if (number1 != undefined) {
+        number1 = displayValue;
+    }
     updateDisplay();
+}
+
+function roundUp(x) {
+    let test = x.toString()
+    if (test.length <= 12) {
+        return x;
+    } else if (test.length > 12) {
+        let cut = test.slice(0, 12);
+        return Number(cut);
+    }
 }
 
 function updateDisplay() {
@@ -114,13 +124,13 @@ function updateDisplay() {
 
 // DOM TARGETING
 const button7 = document.querySelector('#seven');
-button7.addEventListener('click', () => keyPress(7));
+button7.addEventListener('click', () => keyPress("7"));
 
 const button8 = document.querySelector('#eight');
-button8.addEventListener('click', () => keyPress(8));
+button8.addEventListener('click', () => keyPress("8"));
 
 const button9 = document.querySelector('#nine');
-button9.addEventListener('click', () => keyPress(9));
+button9.addEventListener('click', () => keyPress("9"));
 
 const buttonC = document.querySelector('#C');
 buttonC.addEventListener('click', () => deleteLast());
@@ -129,13 +139,13 @@ const buttonAC = document.querySelector('#AC');
 buttonAC.addEventListener('click', () => undoAll());
 
 const button4 = document.querySelector('#four');
-button4.addEventListener('click', () => keyPress(4));
+button4.addEventListener('click', () => keyPress("4"));
 
 const button5 = document.querySelector('#five');
-button5.addEventListener('click', () => keyPress(5));
+button5.addEventListener('click', () => keyPress("5"));
 
 const button6 = document.querySelector('#six');
-button6.addEventListener('click', () => keyPress(6));
+button6.addEventListener('click', () => keyPress("6"));
 
 const buttonMultiply = document.querySelector('#multiply');
 buttonMultiply.addEventListener('click', () => operatorAssignment("multiply"));
@@ -144,13 +154,13 @@ const buttonDivide = document.querySelector('#divide');
 buttonDivide.addEventListener('click', () => operatorAssignment("divide"));
 
 const button1 = document.querySelector('#one');
-button1.addEventListener('click', () => keyPress(1));
+button1.addEventListener('click', () => keyPress("1"));
 
 const button2 = document.querySelector('#two');
-button2.addEventListener('click', () => keyPress(2));
+button2.addEventListener('click', () => keyPress("2"));
 
 const button3 = document.querySelector('#three');
-button3.addEventListener('click', () => keyPress(3));
+button3.addEventListener('click', () => keyPress("3"));
 
 const buttonAdd = document.querySelector('#add');
 buttonAdd.addEventListener('click', () => operatorAssignment("add"));
@@ -159,7 +169,7 @@ const buttonSubtract = document.querySelector('#subtract');
 buttonSubtract.addEventListener('click', () => operatorAssignment("subtract"));
 
 const button0 = document.querySelector('#zero');
-button0.addEventListener('click', () => keyPress(0));
+button0.addEventListener('click', () => keyPress("0"));
 
 const buttonDot = document.querySelector('#dot');
 buttonDot.addEventListener('click', () => dotPress());
